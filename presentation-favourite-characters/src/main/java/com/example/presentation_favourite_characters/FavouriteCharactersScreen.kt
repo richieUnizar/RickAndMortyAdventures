13f0cac978b279bbc3ffa3a43cbae948b5f9d6d9
ricardo.ibanez <ricardo.ibanez@bestsecret.com>
2024-10-07 16:24:58 +0200

    Remove character from favorites screen when removing from favorites from detail view

--- presentation-favourite-characters/src/main/java/com/example/presentation_favourite_characters/FavouriteCharactersScreen.kt
+++ presentation-favourite-characters/src/main/java/com/example/presentation_favourite_characters/FavouriteCharactersScreen.kt
@@ -3,6 +3,7 @@ package com.example.presentation_favourite_characters
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -11,6 +12,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.presentation_base.navigation.NavigationItem
+import com.example.presentation_base.navigation.NavigationItem.Detail.STATUS_OF_FAVOURITE_DETAIL_KEY
+import com.example.presentation_base.navigation.observeLiveData
 import com.example.presentation_base.ui.top_bar.TopBarScaffoldComposable
 import com.example.presentation_favourite_characters.ui.FavouriteCharactersComposable
 import com.example.presentation_favourite_characters.ui.FavouriteMessageComposable
@@ -21,6 +24,15 @@ fun FavouriteCharactersScreen(
 ) {
     val viewModel: FavouriteCharactersViewModel = hiltViewModel()
     val display by viewModel.charactersDisplay.collectAsState()
+
+    val favouriteDetailsChanged =
+        observeLiveData<Int>(navController, STATUS_OF_FAVOURITE_DETAIL_KEY)
+
+    LaunchedEffect(favouriteDetailsChanged) {
+        favouriteDetailsChanged.value?.let { id ->
+            viewModel.removeCharacterFromFavourites(id)
+        }
+    }
 
     if(display.characterList.isEmpty()) {
         FavouriteMessageComposable()
